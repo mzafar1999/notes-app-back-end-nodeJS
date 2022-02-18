0a66c66bc448a82a41baee6580e9c14ba66e1359
mzafar <m.zafarhayatzada@gmail.com>
2022-02-18 21:20:01 +0500

    login endpoint implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const CryptoJS = require("crypto-js");
-var AES = require("crypto-js/aes");
 const dotenv = require('dotenv');
 const User = require('../Models/User');
-const mongoose = require("mongoose");
 dotenv.config()
 router.post("/register", async(req, res) => {
     const { username, email, password } = req.body;
@@ -20,6 +18,33 @@ router.post("/register", async(req, res) => {
     }
 });
 
+
+router.post("/login", async(req, res) => {
+    try {
+        const findUser = await User.findOne({ email: req.body.email.toString() })
+
+        //User does not exist
+        if (!findUser) {
+            res.status(404).json('User did not found!')
+        }
+        const hashedPassword = findUser.password
+
+        //Decrypt Password 
+        const decryptedPassword = CryptoJS.AES.decrypt(hashedPassword, process.env.SEC_KEY_CJS).toString(CryptoJS.enc.Utf8)
+
+        //Send user
+        if (decryptedPassword === req.body.password) {
+            res.status(200).json({ username: findUser.username, email: findUser.email })
+        } else {
+            res.status(401).json('Wrong email or password')
+        }
+
+
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 
 
 
