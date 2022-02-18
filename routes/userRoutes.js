14e0791e1c0e5758120a7a957ec18ba25579c71a
mzafar <m.zafarhayatzada@gmail.com>
2022-02-18 23:32:52 +0500

    JWT Token code written
    Access token for login user implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const CryptoJS = require("crypto-js");
 const dotenv = require('dotenv');
 const User = require('../Models/User');
+const jwt = require('jsonwebtoken')
 dotenv.config()
+
+//Register
 router.post("/register", async(req, res) => {
 
     try {
@@ -23,7 +26,7 @@ router.post("/register", async(req, res) => {
     }
 });
 
-
+//Login
 router.post("/login", async(req, res) => {
     try {
         const findUser = await User.findOne({ email: req.body.email.toString() })
@@ -31,29 +34,32 @@ router.post("/login", async(req, res) => {
         //User does not exist
         if (!findUser) {
             res.status(404).json('User did not found!')
+            return
         }
         const hashedPassword = findUser.password
 
         //Decrypt Password 
         const decryptedPassword = CryptoJS.AES.decrypt(hashedPassword, process.env.SEC_KEY_CJS).toString(CryptoJS.enc.Utf8)
 
-        //Send user
+        //Generate Token
+        const accessToken = jwt.sign({ id: findUser._id, isAdmin: findUser.isAdmin }, process.env.JWT_SECRET_TOKEN, { expiresIn: '15d' })
+            //Send user
         if (decryptedPassword === req.body.password) {
-            res.status(200).json({ username: findUser.username, email: findUser.email })
+            console.log(accessToken);
+            res.status(200).json({ username: findUser.username, email: findUser.email, accessToken })
         } else {
             res.status(401).json('Wrong email or password')
+            return
         }
-
-
 
     } catch (error) {
         res.status(500).json(error)
     }
 });
 
+//Delete
+router.delete('/:id', async(req, res) => {
 
+})
 
-// Decrypt
-// var bytes = CryptoJS.AES.decrypt(hashedPassowrd, process.env.SEC_KEY_CJS);
-// var originalText = bytes.toString(CryptoJS.enc.Utf8);
 module.exports = router;
